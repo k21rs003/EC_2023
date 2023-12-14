63434ba30dc7470b183803e75d36f83c2452da2f
k21rs003 <k21rs003@st.kyusan-u.ac.jp>
2023-12-15 01:14:00 +0900

    htmlとcssで枠組みを作成した。

--- www/script.js
+++ www/script.js
@@ -1,6 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var detailButton = document.querySelector('.btn-action');
-    detailButton.addEventListener('click', function() {
-        console.log('商品詳細を見る');
-    });
+function goToDetail(productId) {
+    // 実際には商品IDに基づいてニフクラバックエンドから商品詳細を取得し、
+    // 詳細画面に遷移する処理を実装します。
+    console.log("商品詳細画面に遷移: ", productId);
+    // 例: window.location.href = 'product-detail.html?id=' + productId;
+}
+
+// 前回のJavaScriptコードに追加
+
+function changeQuantity(change) {
+    var quantityInput = document.getElementById('quantity');
+    var currentQuantity = parseInt(quantityInput.value);
+    currentQuantity += change;
+    if (currentQuantity < 1) currentQuantity = 1; // 数量は1未満にはならない
+    quantityInput.value = currentQuantity;
+}
+
+// 購入ボタンのイベントリスナー（仮の例）
+document.querySelector('.purchase-btn').addEventListener('click', function() {
+    var quantity = document.getElementById('quantity').value;
+    alert('購入する数量: ' + quantity);
+    // ここでニフクラバックエンドに購入情報を送信する処理を実装します
 });
