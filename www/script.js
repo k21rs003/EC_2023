c2c8f3e348b4f6f44c03a39d7643fc5cecb70b02
k21rs003 <k21rs003@st.kyusan-u.ac.jp>
2023-12-14 13:44:56 +0900

    主な配置を追加した。

---
+++ www/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var detailButton = document.querySelector('.btn-action');
+    detailButton.addEventListener('click', function() {
+        console.log('商品詳細を見る');
+    });
+});
