23810ea4e59695acc31b51becfcd1201da0c1c68
k21rs003 <k21rs003@st.kyusan-u.ac.jp>
2023-12-15 11:10:46 +0900

    デザインの大枠と商品写真のアップロードを行った。

--- www/script.js
+++ www/script.js
@@ -1,11 +1,8 @@
 function goToDetail(productId) {
-    // 実際には商品IDに基づいてニフクラバックエンドから商品詳細を取得し、
-    // 詳細画面に遷移する処理を実装します。
+    // 詳細画面に遷移する処理を実装。
     console.log("商品詳細画面に遷移: ", productId);
     // 例: window.location.href = 'product-detail.html?id=' + productId;
 }
-
-// 前回のJavaScriptコードに追加
 
 function changeQuantity(change) {
     var quantityInput = document.getElementById('quantity');
@@ -15,9 +12,9 @@ function changeQuantity(change) {
     quantityInput.value = currentQuantity;
 }
 
-// 購入ボタンのイベントリスナー（仮の例）
+// 購入ボタンのイベントリスナー
 document.querySelector('.purchase-btn').addEventListener('click', function() {
     var quantity = document.getElementById('quantity').value;
     alert('購入する数量: ' + quantity);
-    // ここでニフクラバックエンドに購入情報を送信する処理を実装します
+    // ここでニフクラバックエンドに購入情報を送信する処理を実装
 });
