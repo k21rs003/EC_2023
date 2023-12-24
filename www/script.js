86ab67313a6a1ae6defa81bc2d49e141dc49af6f
k21rs003 <k21rs003@st.kyusan-u.ac.jp>
2023-12-24 14:38:57 +0900

    商品情報詳細画面のカートに入れるボタンを押すと、表示されるアラート数を追加した。

--- www/script.js
+++ www/script.js
@@ -2,21 +2,32 @@ let ncmb = new NCMB(appKey, clientKey);
 let UTdata = ncmb.DataStore("Cart");
 
 function enterData() {
-    let uTdata = new UTdata();
     let productName = document.getElementById('productName').textContent;
     let quantity = parseInt(document.getElementById('quantity').value);
     let unitPriceElement = document.getElementById('unitPrice');
     let unitPriceValue = unitPriceElement.getAttribute('value');
+    let subtotal = parseInt(unitPriceValue) * quantity;
 
-    uTdata.set("productName", productName);
-    uTdata.set("quantity", quantity);
-    uTdata.set("unitPrice", parseInt(unitPriceValue));
-    uTdata.set("subtotal", parseInt(unitPriceValue) * quantity);
-    uTdata.save()
-        .then(function(results) {
-            alert('商品名: ' + productName + '\n購入する数量: ' + quantity);
-        })
-        .catch(function(results) {
-            alert("error");
-        });
+    // 確認ダイアログを表示
+    let confirmMessage = 'カートに入れますか？' + '\n商品名: ' + productName + '\n数量: ' + quantity + '\n単価: ¥' + unitPriceValue + '\n合計金額: ¥' + subtotal;
+    let confirmAddToCart = confirm(confirmMessage);
+    
+    if (confirmAddToCart) {
+        // ユーザーが「はい」を選択した場合、データを保存
+        let uTdata = new UTdata();
+        uTdata.set("productName", productName);
+        uTdata.set("quantity", quantity);
+        uTdata.set("unitPrice", parseInt(unitPriceValue));
+        uTdata.set("subtotal", subtotal);
+
+        uTdata.save()
+            .then(function(results) {
+                alert('カートに追加しました！');
+            })
+            .catch(function(results) {
+                alert("エラーが発生しました");
+            });
+    } else {
+        // ユーザーが「キャンセル」を選択した場合、何もしない
+    }
 }
